b00deccab9bda6c27127be66f35dd24a2e3b799e
Armando del Angel <113638993+aarmando2@users.noreply.github.com>
2023-11-10 02:58:41 -0600

    Change icon when is acrive

--- Acciones/taps.js
+++ Acciones/taps.js
@@ -1,6 +1,7 @@
-// Func Tap Control Formato
-function selectFormat(evt, format) {
-  hide_show_dropdawn(format);
+// Func Tap Control vistao
+function select_vista(evt, vista) {
+  hide_show_dropdawn(vista);
+  change_img(vista);
   clean_cards();
   // Declare all variables
   var i, tabcontent, tablinks;
@@ -18,14 +19,14 @@ function selectFormat(evt, format) {
   }
 
   // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(format).style.display = "block";
+  document.getElementById(vista).style.display = "block";
   evt.currentTarget.className += " active";
 }
 
 document.getElementById("defaultOpen").click();
 
 // Funct Tap Control Vista (evaluacion o resumen)
-function selectVista(evt, vista) {
+function select_card(evt, vista) {
   btn_evaluacion_card(vista);
   // Declare all variables
   var i, tabcontent, tablinks;
@@ -49,12 +50,12 @@ function selectVista(evt, vista) {
 
 document.getElementById("btn-ver-resumen").click();
 
-// Muestra y quita los dropdawns cuando seleccionas el formato de vista
-function hide_show_dropdawn(format) {
-  if (format === "vista-tabla") {
+// Muestra y quita los dropdawns cuando seleccionas el vistao de vista
+function hide_show_dropdawn(vista) {
+  if (vista === "vista-tabla") {
     document.getElementById("dropdawn-ordenamiento").style.display = "block";
     document.getElementById("dropdawn-dimension").style.display = "none";
-  } else if (format === "vista-mapa") {
+  } else if (vista === "vista-mapa") {
     document.getElementById("dropdawn-ordenamiento").style.display = "none";
     document.getElementById("dropdawn-dimension").style.display = "block";
   }
@@ -67,5 +68,15 @@ function btn_evaluacion_card(vista) {
   } else if (vista === "vista-evaluacion") {
     document.getElementById("btn-ver-resumen").style.display = "block";
     document.getElementById("btn-ver-evaluacion").style.display = "none";
+  }
+}
+
+function change_img(vista) {
+  if (vista === "vista-tabla") {
+    $("#img_tabla").attr("src", "src/tabla_active.svg");
+    $("#img_mapa").attr("src", "src/map_default.svg");
+  } else if (vista === "vista-mapa") {
+    $("#img_tabla").attr("src", "src/tabla_default.svg");
+    $("#img_mapa").attr("src", "src/map_active.svg");
   }
 }
